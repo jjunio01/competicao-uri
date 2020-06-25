0e8855724acc5fe29562adec8b9bef0f2e31a13f
jjunio01 <junio.araujo01@yahoo.com>
2020-06-24 22:37:57 -0300

    Desafio 1002

---
+++ 1002.py
@@ -0,0 +1,9 @@
+'''     Área de um círculo.
+    Fórmula para calcular a área do círculo:  A = π . R. R
+    π = 3.14159
+    O valor de entrada deverá ter duas casas decimais
+'''
+R = float(input())
+A = 3.14159 * R * R
+print('A={:.4f}'.format(A))
+
